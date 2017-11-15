fbf6679b8b8a3e991c9269a76533edbf833e952d
Anurag Dwivedi <anurag.dwivedi@oracle.com>
2017-11-15 22:28:41 +0530

    Added newrelics

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Laundrynerds'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '741bec5190bd27d10ef7c053f16a0e520350d8df',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
