f81a7a8aba50bcb7038652d21acdae520b511a6b
Anurag Dwivedi <anurag.dwivedi@oracle.com>
2017-12-16 21:52:57 +0530

    Adding config file

---
+++ config.js
@@ -0,0 +1,33 @@
+var config = {
+	origin: 'localhost',
+	originPort: 4444,
+	appName: 'lnapp',
+	key: 'ln-sessionkey',
+	mongoConfig: {
+		server: 'localhost',
+		port: 27017,
+		db: 'laundrynerds',
+		options: {
+			db: {},
+			server: {
+				poolSize: 2,
+				socketOptions: {
+					keepAlive: 1
+				}
+			}
+		}
+	},
+	zohoConfig: {
+		host: 'smtp.zoho.com1',
+		port: 465,
+		secure: true,
+		user: 'support@laundrynerds.com1',
+		pass: 'test1'
+	},
+	mailgunConfig: {
+		apiKey: 'key-test',
+		domain: 'ln.com'
+	}
+};
+
+module.exports = config;
