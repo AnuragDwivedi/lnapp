e0876896bf42af8376eed3a31266e48349fa7d3c
anurag.dwivedi <anurag.dwivedi@servicenow.com>
2018-09-01 13:41:48 +0530

    Updating mongoose push all flag

--- app/models/CommercialLeadModel.js
+++ app/models/CommercialLeadModel.js
@@ -84,6 +84,8 @@ var CommercialLeadSchema = new Schema({
 		type: Date,
 		"default": Date.now
 	}
+}, { 
+	usePushEach: true 
 });
 
 module.exports = mongoose.model('CommercialLead', CommercialLeadSchema);
